ba3a7ea700f4777ea4695d4370b9b80259eca7e1
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-09-07 01:25:56 +0100

    version constraints for provider

--- provider.tf
+++ provider.tf
@@ -2,9 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.9.0"
+      version = ">= 6.9.0, < 7.0.0"
+
     }
   }
+
+  required_version = ">= 1.6.0"
 
   # use remote state file
   backend "s3" {
